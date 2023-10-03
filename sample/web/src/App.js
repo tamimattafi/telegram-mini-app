276d8235ab66cd82c572ef173112f9e2044f23dd
Tamim Attafi <tamim.attafi@i-link.pro>
2023-10-04 05:20:27 +0700

    config: init sample react.js app

---
+++ sample/web/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hi</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
