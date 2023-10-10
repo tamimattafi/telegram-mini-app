0c3320cc3b374d9d3f89b0337eb0afe80a26fdcf
Tamim Attafi <tamim.attafi@i-link.pro>
2023-10-11 02:20:49 +0700

    feat: add send data and query handling

--- sample/backend/src/telegram/Bot.js
+++ sample/backend/src/telegram/Bot.js
@@ -2,6 +2,7 @@
 import { Telegraf } from 'telegraf'
 import { message } from 'telegraf/filters'
 
+
 /**
  * Creates and launches Telegram bot, and assigns all the required listeners
  *
@@ -19,6 +20,7 @@ export function launchBot(token){
     listenToCommands(bot)
     listenToMessages(bot)
     listenToQueries(bot)
+    listenToMiniAppData(bot)
 
     // Launch the bot
     bot.launch().then(() => console.log('bot launched'))
@@ -38,10 +40,17 @@ export function launchBot(token){
 function listenToCommands(bot) {
     // Register a listener for the /start command, and reply with a message whenever it's used
     bot.start(async (ctx) => {
-        ctx.reply("Welcome to MiniAppSample bot! Click on the button below to launch our mini app", {
+        await ctx.reply("Welcome to MiniAppSample bot!", {
+            reply_markup: {
+                keyboard: [
+                    [ { text: "Start Mini App", web_app: { url: process.env.APP_URL } } ],
+                ]
+            }
+        })
+
+        await ctx.reply("Click on the button below to launch our mini app", {
             reply_markup: {
                 inline_keyboard: [
-                    /* Inline buttons. 2 side-by-side */
                     [ { text: "Start Mini App", web_app: { url: process.env.APP_URL } } ],
                 ]
             }
@@ -50,7 +59,7 @@ function listenToCommands(bot) {
 
     // Register a listener for the /help command, and reply with a message whenever it's used
     bot.help(async (ctx) => {
-        ctx.reply("Run the /start command to use our mini app")
+        await ctx.reply("Run the /start command to use our mini app")
     })
 }
 
@@ -63,20 +72,43 @@ function listenToCommands(bot) {
 function listenToMessages(bot) {
     // Listen to messages and reply with something when ever you receive them
     bot.hears("hi", async (ctx) => {
-        ctx.reply('Hey therez')
+        await ctx.reply('Hey there!')
     })
 
     // Listen to messages with the type 'sticker' and reply whenever you receive them
     bot.on(message("text"), async (ctx) => {
-        ctx.reply("I don't understand text but I like stickers, send me some!")
-        ctx.reply("Or you can send me one of these commands \n/start\n/help")
+        await ctx.reply("I don't understand text but I like stickers, send me some!")
+        await ctx.reply("Or you can send me one of these commands \n/start\n/help")
     });
 
     // Listen to messages with the type 'sticker' and reply whenever you receive them
     bot.on(message("sticker"), async (ctx) => {
-        ctx.reply("I like your sticker! 🔥")
+        await ctx.reply("I like your sticker! 🔥")
     })
 }
+
+
+/**
+ * Listen to messages send by MiniApp through sendData(data)
+ * @see https://core.telegram.org/bots/webapps#initializing-mini-apps
+ *
+ * @param bot Telegraf bot instance
+ */
+function listenToMiniAppData(bot) {
+    bot.on("message", async (ctx) => {
+        if (ctx.message?.web_app_data?.data) {
+            try {
+                const data = ctx.message?.web_app_data?.data
+                await bot.sendMessage(ctx.message.chat.id, 'Got message from MiniApp')
+                await bot.sendMessage(ctx.message.chat.id, data)
+            } catch (e) {
+                await bot.sendMessage(ctx.message.chat.id, 'Got message from MiniApp but failed to read')
+                await bot.sendMessage(ctx.message.chat.id, e)
+            }
+        }
+    });
+}
+
 
 /**
  * Assigns query listeners such inlines and callbacks
@@ -94,7 +126,16 @@ function listenToQueries(bot) {
     })
 
     bot.on("inline_query", async (ctx) => {
-        const result = []
+        const article = {
+            type: 'article',
+            id: ctx.inlineQuery.id,
+            title: 'Message for query',
+            input_message_content: {
+                message_text: `Message for query`
+            }
+        }
+
+        const result = [article]
         // Explicit usage
         await ctx.telegram.answerInlineQuery(ctx.inlineQuery.id, result)
 
