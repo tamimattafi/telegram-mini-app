bf2422d4872b31bbef460770f10669a1a92a8316
Tamim Attafi <tamim.attafi@i-link.pro>
2023-10-10 23:26:09 +0700

    feat: add sendMessageToServerCall component

---
+++ sample/web/src/components/app/server/SendMessageToServerCall.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import TelegramMiniForm from "../../kit/MiniForm/TelegramMiniForm";
+import {useTelegram} from "../../../hooks/useTelegram";
+
+
+/**
+ * /messages endpoint is used to send messages to the bot server using a REST API.
+ * The bot will receive the message and echo it in the chat.
+ *
+ * @see sendMessageToServer
+ */
+const SendMessageToServerCall = () => {
+    const {webApp} = useTelegram()
+
+    // This id will be used by our backend to send messages to the chat with the user
+    const queryId = webApp.initDataUnsafe?.query_id
+
+    const onSendMessage = (message) => {
+        sendMessageToServer(message, queryId)
+    }
+
+    return (
+        <TelegramMiniForm
+            fieldlabel={'/messages'}
+            fielddescription={
+                'This endpoint is used to send messages to the bot server using a REST API. ' +
+                'The bot will receive the message and echo it in the chat'
+            }
+            fieldhint={`Enter a message`}
+            buttonlabel={'Execute'}
+            onSubmit={onSendMessage}
+        />
+    );
+};
+
+export default SendMessageToServerCall;
+
---
+++ sample/web/src/logic/server/HttpClient.js
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+
+
+/**
+ * Uses simple fetch() to send message to the bot server using REST API
+ *
+ * @param message the message to be sent
+ * @param queryId get from webApp.initDataUnsafe?.query_id
+ */
+function sendMessageToServer(message, queryId) {
+
+}
